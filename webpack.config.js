7b675545adb34f905ee1af3f890debfbd1d33512
vladimir <lovkiy2012@gmail.com>
2020-05-31 00:44:05 +0300

    31 fix webpack.config CopyPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -55,12 +55,14 @@ module.exports = {
         collapseWhitespace: isProd,
       },
     }),
-    new CopyPlugin([
-      {
-        from: path.resolve(__dirname, 'src/favicon.ico'),
-        to: path.resolve(__dirname, 'dist'),
-      },
-    ]),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
     new MiniCssExtractPlugin({
       filename: filename('css'),
     }),
