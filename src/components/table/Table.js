4dd8e9e6fa92fe2a34db182e92dd79616424f1d3
vladimir <lovkiy2012@gmail.com>
2020-06-02 21:49:28 +0300

    37 create table. Headers

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,30 +1,8 @@
 import {ExcelComponent} from '../../core/ExcelComponent'
+import {createTable} from './table.template'
 export class Table extends ExcelComponent {
   static className = 'excel__table'
   toHTML() {
-    return ` <div class="row">
-    <div class="row-info"></div>
-    <div class="row-data">
-      <div class="column">A</div>
-      <div class="column">B</div>
-      <div class="column">C</div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="row-info">1</div>
-    <div class="row-data">
-      <div class="cell selected" contenteditable>a1</div>
-      <div class="cell" contenteditable>b2</div>
-      <div class="cell" contenteditable>c3</div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="row-info">2</div>
-    <div class="row-data">
-      <div class="cell">a1</div>
-      <div class="cell">b2</div>
-      <div class="cell">c3</div>
-    </div>
-  </div>`
+    return createTable()
   }
 }
