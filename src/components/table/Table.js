a7f8c23c896b07a5a1083227451a67c95d524c1f
vladimir <lovkiy2012@gmail.com>
2020-06-03 20:31:22 +0300

    41 algorithm of actions and events

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,8 +1,28 @@
 import {ExcelComponent} from '../../core/ExcelComponent'
 import {createTable} from './table.template'
 export class Table extends ExcelComponent {
+  constructor($root) {
+    super($root, {
+      listeners: ['click', 'mousedown', 'mousemove', 'mouseup'],
+    })
+  }
   static className = 'excel__table'
   toHTML() {
     return createTable()
+  }
+  onClick() {
+    console.log('click')
+  }
+
+  onMousedown(event) {
+    console.log('mousedown', event.target)
+  }
+
+  onMousemove() {
+    console.log('mousemove')
+  }
+
+  onMouseup() {
+    console.log('mouseup')
   }
 }
