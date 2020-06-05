9f6c981588386ce5b0bb02c676cab575907e0bd4
vladimir <lovkiy2012@gmail.com>
2020-06-05 23:23:31 +0300

    49 significant optimization

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -14,30 +14,40 @@ export class Table extends ExcelComponent {
   onMousedown(event) {
     if (event.target.dataset.resize) {
       const $resizer = $(event.target)
-      // const $parent = $resizer.$el.parentNode
+
       const $parent = $resizer.closest('[data-type="resizable"]')
 
       const coords = $parent.getCoords()
-      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+
       const type = $resizer.data.resize
-      console.log('type  :>> ', type)
+      const sideProp = type === 'col' ? 'bottom' : 'right'
+      let value
+      $resizer.css({opacity: 1, [sideProp]: '-5000px'})
       document.onmousemove = (e) => {
         if (type === 'col') {
           const delta = e.pageX - coords.right
-          const value = coords.width + delta
-          $parent.css({width: value + 'px'})
-          // $parent.$el.style.width = value + 'px'
-          cells.forEach((el) => (el.style.width = value + 'px'))
+
+          value = coords.width + delta
+          $resizer.css({right: -delta + 'px'})
         } else {
           const delta = e.pageY - coords.bottom
-          const value = coords.height + delta
-          $parent.css({height: value + 'px'})
-          // $parent.$el.style.height = value + 'px'
-          console.log('delta :>> ', delta)
+          value = coords.height + delta
+          $resizer.css({bottom: -delta + 'px'})
         }
       }
       document.onmouseup = () => {
         document.onmousemove = null
+        document.onmouseup = null
+        if (type === 'col') {
+          $parent.css({width: value + 'px'})
+          this.$root
+            .findAll(`[data-col="${$parent.data.col}"]`)
+            .forEach((el) => (el.style.width = value + 'px'))
+        } else {
+          $parent.css({height: value + 'px'})
+        }
+
+        $resizer.css({opacity: 0, bottom: 0, right: 0})
       }
     }
   }
