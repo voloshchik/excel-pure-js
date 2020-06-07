4ec73a758834b28d0afd66ec7c33b865f11f2a54
vladimir <lovkiy2012@gmail.com>
2020-06-07 20:15:01 +0300

    61 creating interactive

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -9,7 +9,7 @@ export class Table extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Table',
-      listeners: ['mousedown', 'keydown'],
+      listeners: ['mousedown', 'keydown', 'input'],
       ...options,
     })
   }
@@ -24,11 +24,17 @@ export class Table extends ExcelComponent {
     super.init()
 
     const $cell = this.$root.find('[data-id="0:0"]')
-    this.selection.select($cell)
+    this.selectCell($cell)
     this.$on('formula:input', (text) => {
       this.selection.current.text(text)
-      console.log('Table from formula :>> ', text)
     })
+    this.$on('formula:done', () => {
+      this.selection.current.focus()
+    })
+  }
+  selectCell($cell) {
+    this.selection.select($cell)
+    this.$emit('table:select', $cell)
   }
   onMousedown(event) {
     if (shouldResize(event)) {
@@ -62,8 +68,10 @@ export class Table extends ExcelComponent {
       event.preventDefault()
       const id = this.selection.current.id(true)
       const $next = this.$root.find(nextSelector(key, id))
-      this.selection.select($next)
-      console.log('key :>> ', key)
+      this.selectCell($next)
     }
+  }
+  onInput(event) {
+    this.$emit('table:input', $(event.target))
   }
 }
