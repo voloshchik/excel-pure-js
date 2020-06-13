2312221c712d051e94b68a05979d016931aaad6b
vladimir <lovkiy2012@gmail.com>
2020-06-13 19:59:08 +0300

    73 creating toolbar

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,4 +1,6 @@
 import {ExcelComponent} from '../../core/ExcelComponent'
+import {createToolbar} from './toolbar.tamplate'
+import {$} from '@core/dom'
 export class Toolbar extends ExcelComponent {
   static className = 'excel__toolbar'
   constructor($root, options) {
@@ -9,31 +11,12 @@ export class Toolbar extends ExcelComponent {
     })
   }
   toHTML() {
-    return `<div class="button">
-    <i class="material-icons">format_align_left</i>
-  </div>
-
-  <div class="button">
-    <i class="material-icons">format_align_center</i>
-  </div>
-
-  <div class="button">
-    <i class="material-icons">format_align_right</i>
-  </div>
-
-  <div class="button">
-    <i class="material-icons">format_bold</i>
-  </div>
-
-  <div class="button">
-    <i class="material-icons">format_italic</i>
-  </div>
-
-  <div class="button">
-    <i class="material-icons">format_underlined</i>
-  </div>`
+    return createToolbar()
   }
   onClick(event) {
-    console.log('event.target', event.target)
+    const $target = $(event.target)
+    if ($target.data.type === 'button') {
+      console.log($target.data.value)
+    }
   }
 }
---
+++ src/components/toolbar/toolbar.tamplate.js
@@ -0,0 +1,33 @@
+function toButton(button) {
+  console.log('button.value', button.value)
+  const meta = `
+  data-type = "button"
+  data-value = '${JSON.stringify(button.value)}'
+  `
+  return `<div 
+   ${meta}
+    class="button ${button.active ? 'active' : ''}"
+  >
+    <i
+     class="material-icons"
+     ${meta}
+     >${button.icon}</i>
+  </div>`
+}
+
+export function createToolbar() {
+  const button = [
+    {icon: 'format_align_left', active: false, value: {textAlign: 'left'}},
+    {icon: 'format_align_center', active: false, value: {textAlign: 'center'}},
+    {icon: 'format_align_right', active: false, value: {textAlign: 'right'}},
+    {icon: 'format_bold', active: false, value: {fontWeight: 'bold'}},
+    {icon: 'format_italic', active: false, value: {fontStyle: 'italic'}},
+    {
+      icon: 'format_underlined',
+      active: false,
+      value: {textDecoration: 'underline'},
+    },
+  ]
+
+  return button.map(toButton).join('')
+}
