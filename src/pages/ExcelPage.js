691580263b52c389cd032837ac0164ba92825925
vladimir <lovkiy2012@gmail.com>
2020-06-20 18:29:37 +0300

    88 creating different table models

--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -1,19 +1,26 @@
 import {Page} from '../core/Page'
 import {createStore} from '../core/createStore'
 import {rootReducer} from '../redux/rootReducer'
-import {initialState} from '../redux/initialState'
+import {initialState, normalizeInitialState} from '../redux/initialState'
 import {debounce, storage} from '../core/utils'
 import {Excel} from '../components/excel/Excel'
 import {Header} from '../components/header/Header'
 import {Toolbar} from '../components/toolbar/Toolbar'
 import {Formula} from '../components/formula/Formula'
 import {Table} from '../components/table/Table'
+
+function storageName(param) {
+  return 'excel:' + param
+}
+
 export class ExcelPage extends Page {
   getRoot() {
     console.log('this.params', this.params)
-    const store = createStore(rootReducer, initialState)
+    const params = this.params ? this.params : Date.now().toString()
+    const state = storage(storageName(params))
+    const store = createStore(rootReducer, normalizeInitialState(state))
     const stateListeners = debounce((state) => {
-      storage('excel-state', state)
+      storage(storageName(params), state)
       //   console.log('Appstate', state)
     }, 1000)
 
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,5 +1,5 @@
-import {storage} from '../core/utils'
 import {defaultStyles, defaultTitle} from '../constants'
+import {clone} from '../core/utils'
 const defaultState = {
   title: defaultTitle,
   colState: {},
@@ -13,6 +13,6 @@ const normalize = (state) => {
   return {...state, currentStyles: defaultStyles, currentText: ''}
 }
 
-export const initialState = storage('excel-state')
-  ? normalize(storage('excel-state'))
-  : defaultState
+export function normalizeInitialState(state) {
+  return state ? normalize(state) : clone(defaultState)
+}
